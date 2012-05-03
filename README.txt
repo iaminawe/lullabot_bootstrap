d6c8726752fb3c342bb6827c85122481e21601d1
Brock Boland <brock@brockboland.com>
2012-05-03 16:45:42 -0400

    Overwritten with a fork of the twitter_bootstrap theme

---
+++ README.txt
@@ -0,0 +1,7 @@
+Twitter Bootstrap theme for Drupal
+
+1. Download the Bootstrap library v2 (http://twitter.github.com/bootstrap/), make sure the resulting folder is named "bootstrap"
+2. Put the bootstrap folder inside the lullabot_bootstrap theme folder, [path the themes]/lullabot_bootstrap/bootstrap/js…
+3. Put any plugin files into [path the themes]/lullabot_bootstrap/bootstrap/js/
+
+Author: Sjoerd Arendsen
--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,144 +1,94 @@
-<header id="header">
+<header id="navbar" role="banner" class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container">
+      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      </a>
 
-  <div class="container">
-    <div id="header-top" class="row visible-desktop">
-      <div class="span3">
-        <?php if ($logo): ?>
+      <?php if ($logo): ?>
         <a class="brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
           <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
         </a>
-        <?php endif; ?>
-
-      </div>
-
-
-      <?php if ($page['header']): ?>
-      <div class="span9">
-        <?php print render($page['header']); ?>
-      </div>  <!-- /#header -->
       <?php endif; ?>
 
+      <?php if ($site_name || $site_slogan): ?>
+        <hgroup id="site-name-slogan">
+          <?php if ($site_name): ?>
+          <h1>
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="brand"><?php print $site_name; ?></a>
+          </h1>
+          <?php endif; ?>
+        </hgroup>
+      <?php endif; ?>
+      
+      <div class="nav-collapse">
+        <nav role="navigation">
+          <?php if ($primary_nav): ?>
+            <?php print $primary_nav; ?>
+          <?php endif; ?>
 
-    </div>
-
-
-
-    <div id="nav" role="banner" class="navbar">
-      <div class="navbar-inner">
-
-        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-        <div class="row">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          </a>
-        </div>
-
-
-        <div class="nav-collapse">
-          <nav role="navigation">
-            <?php if ($primary_nav): ?>
-              <?php print $primary_nav; ?>
-            <?php endif; ?>
-
-            <?php if ($search): ?>
-              <div id="nav-search">
-                <?php if ($search): print render($search); endif; ?>
-              </div>
-            <?php endif; ?>
-
-          </nav>
-        </div>
-
+          <?php if ($secondary_nav): ?>
+            <?php print $secondary_nav; ?>
+          <?php endif; ?>
+        </nav>
       </div>
     </div>
   </div>
 </header>
 
-<div id="main">
-  <div class="container">
-    <?php if ($page['hero']): ?>
-      <div id="hero">
-      <?php print render($page['hero']); ?>
-      </div>
+<div class="container">
+
+  <header role="banner" id="page-header">
+    <?php if ($site_slogan ): ?>
+      <p class="lead"><?php print $site_slogan; ?></p>
     <?php endif; ?>
 
+    <?php print render($page['header']); ?>
+  </header> <!-- /#header -->
 
+  <div class="row">
 
+    <?php if ($page['sidebar_first']): ?>
+      <aside class="span3" role="complementary">
+        <?php print render($page['sidebar_first']); ?>
+      </aside>  <!-- /#sidebar-first -->
+    <?php endif; ?>  
 
-    <div class="row">
-
-
-      <?php if ($page['sidebar_first']): ?>
-        <aside id="primary-sidebar-region" class="span4" role="complementary">
-          <div id="primary-sidebar-wrapper" class="sidebar-wrapper">
-            <?php print render($page['sidebar_first']); ?>
-          </div>
-        </aside>  <!-- /#sidebar-first -->
+    <section class="<?php print _lullabot_bootstrap_content_span($columns); ?>">
+      <?php if ($page['highlighted']): ?>
+        <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
-
-      <section  id="main-content-region" class="<?php print lullabot_bootstrap_content_span($columns); ?>">
-        <div id="main-content-wrapper">
-          <a id="main-content"></a>
-          <?php print render($title_prefix); ?>
-          <?php if ($title): ?>
-            <h1 class="page-header"><?php print $title; ?></h1>
-          <?php endif; ?>
-          <?php print render($title_suffix); ?>
-          <?php print $messages; ?>
-          <?php if ($tabs): ?>
-            <?php print render($tabs); ?>
-          <?php endif; ?>
-          <?php if ($action_links): ?>
-            <ul class="action-links"><?php print render($action_links); ?></ul>
-          <?php endif; ?>
-          <?php print render($page['content']); ?>
-        </div>
-      </section>
-
-      <?php if ($page['sidebar_second']): ?>
-        <aside id="secondary-sidebar-region" class="span4" role="complementary">
-          <div id="secondary-sidebar-wrapper" class="sidebar-wrapper">
-            <?php print render($page['sidebar_second']); ?>
-          </div>
-        </aside>  <!-- /#sidebar-second -->
+      <?php if ($breadcrumb): print $breadcrumb; endif;?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="page-header"><?php print $title; ?></h1>
       <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php if ($tabs): ?>
+        <?php print render($tabs); ?>
+      <?php endif; ?>
+      <?php if ($page['help']): ?> 
+        <div class="well"><?php print render($page['help']); ?></div>
+      <?php endif; ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+    </section>
 
-    </div>
-
-    <?php if ($page['content_bottom']): ?>
-    <div class="row">
-      <div id="content-bottom-region" class="span12">
-      <?php print render($page['content_bottom']); ?>
-      </div>
-    </div>
-
+    <?php if ($page['sidebar_second']): ?>
+      <aside class="span3" role="complementary">
+        <?php print render($page['sidebar_second']); ?>
+      </aside>  <!-- /#sidebar-second -->
     <?php endif; ?>
-
-
-
 
   </div>
+  <footer class="footer container">
+    <?php print render($page['footer']); ?>
+  </footer>
 </div>
-
-<footer id="footer" class="footer">
-  <div class="container">
-    <div class="row">
-      <div class="span6">
-        <?php if ($page['footer']): ?>
-          <?php print render($page['footer']); ?>
-        <?php endif; ?>
-      </div>
-      <div class="span6">
-        <?php if ($page['footer_menu']): ?>
-          <?php print render($page['footer_menu']); ?>
-        <?php endif; ?>
-      </div>
-    </div>
-  </div>
-</footer>
-
-
-	
-
