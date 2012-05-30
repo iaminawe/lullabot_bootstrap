5cef52c702e350a67da78e3ed5f731cfdeb8d0c5
Brock Boland <brock@brockboland.com>
2012-05-29 21:17:43 -0400

    Check if the menu module is enabled before calling menu functions (#3)

--- template.php
+++ template.php
@@ -102,56 +102,59 @@ function lullabot_bootstrap_preprocess_page(&$variables) {
     $variables['columns'] = 1;
   }
   
-  // Primary nav
-  $variables['primary_nav'] = FALSE;
-  if ($variables['main_menu']) {
-    // Build links
-    $tree = menu_tree_page_data(variable_get('menu_main_links_source', 'main-menu'));
-    $variables['main_menu'] = lullabot_bootstrap_menu_navigation_links($tree);
+  // Make sure the menu module is in use before setting menu vars.
+  if (module_exists('menu')) {
+    // Primary nav
+    $variables['primary_nav'] = FALSE;
+    if ($variables['main_menu']) {
+      // Build links
+      $tree = menu_tree_page_data(variable_get('menu_main_links_source', 'main-menu'));
+      $variables['main_menu'] = lullabot_bootstrap_menu_navigation_links($tree);
     
-    // Build list
-    $variables['primary_nav'] = theme('lullabot_bootstrap_links', array(
-      'links' => $variables['main_menu'],
-      'attributes' => array(
-        'id' => 'main-menu',
-        'class' => array('nav'),
-      ),
-      'heading' => array(
-        'text' => t('Main menu'),
-        'level' => 'h2',
-        'class' => array('element-invisible'),
-      ),
-    ));
-  }
+      // Build list
+      $variables['primary_nav'] = theme('lullabot_bootstrap_links', array(
+        'links' => $variables['main_menu'],
+        'attributes' => array(
+          'id' => 'main-menu',
+          'class' => array('nav'),
+        ),
+        'heading' => array(
+          'text' => t('Main menu'),
+          'level' => 'h2',
+          'class' => array('element-invisible'),
+        ),
+      ));
+    }
   
-  // Secondary nav
-  $variables['secondary_nav'] = FALSE;
-  if ($variables['secondary_menu']) {
-    $secondary_menu = menu_load(variable_get('menu_secondary_links_source', 'user-menu'));
+    // Secondary nav
+    $variables['secondary_nav'] = FALSE;
+    if ($variables['secondary_menu']) {
+      $secondary_menu = menu_load(variable_get('menu_secondary_links_source', 'user-menu'));
     
-    // Build links
-    $tree = menu_tree_page_data($secondary_menu['menu_name']);
-    $variables['secondary_menu'] = lullabot_bootstrap_menu_navigation_links($tree);
+      // Build links
+      $tree = menu_tree_page_data($secondary_menu['menu_name']);
+      $variables['secondary_menu'] = lullabot_bootstrap_menu_navigation_links($tree);
     
-    // Build list
-    $variables['secondary_nav'] = theme('lullabot_bootstrap_btn_dropdown', array(
-      'links' => $variables['secondary_menu'],
-      'label' => $secondary_menu['title'],
-      'type' => 'success',
-      'attributes' => array(
-        'id' => 'user-menu',
-        'class' => array('pull-right'),
-      ),
-      'heading' => array(
-        'text' => t('Secondary menu'),
-        'level' => 'h2',
-        'class' => array('element-invisible'),
-      ),
-    ));
+      // Build list
+      $variables['secondary_nav'] = theme('lullabot_bootstrap_btn_dropdown', array(
+        'links' => $variables['secondary_menu'],
+        'label' => $secondary_menu['title'],
+        'type' => 'success',
+        'attributes' => array(
+          'id' => 'user-menu',
+          'class' => array('pull-right'),
+        ),
+        'heading' => array(
+          'text' => t('Secondary menu'),
+          'level' => 'h2',
+          'class' => array('element-invisible'),
+        ),
+      ));
+    }
+
+    // Replace tabs with dropw down version
+    $variables['tabs']['#primary'] = _lullabot_bootstrap_local_tasks($variables['tabs']['#primary']);
   }
-  
-  // Replace tabs with dropw down version
-  $variables['tabs']['#primary'] = _lullabot_bootstrap_local_tasks($variables['tabs']['#primary']);
 }
 
 
